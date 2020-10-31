e68d1728eda939e13832919ee986f57179099291
João Graça <joao.g.n91@gmail.com>
2020-10-31 16:52:06 -0300

    feat: play video

--- public/bandersnatch/index.js
+++ public/bandersnatch/index.js
@@ -9,9 +9,11 @@ async function main() {
     const host = isLocal ? manifestJSON.localHost : manifestJSON.productionHost;
     const videoComponent = new VideoComponent();
     const network = new Network({ host });
-    const videoPlayer = new VideoMediaPlayer({ manifestJSON, network });
+    const videoPlayer = new VideoMediaPlayer({ manifestJSON, network, videoComponent });
     videoPlayer.initializeCodec();
     videoComponent.initializePlayer();
+
+    window.nextChunk = (data) => videoPlayer.nextChunk(data);
 
 }
 
--- public/bandersnatch/src/videoPlayer.js
+++ public/bandersnatch/src/videoPlayer.js
@@ -1,11 +1,14 @@
 class VideoMediaPlayer {
-  constructor({ manifestJSON, network }) {
+  constructor({ manifestJSON, network, videoComponent }) {
     this.manifestJSON = manifestJSON;
     this.network = network;
+    this.videoComponent = videoComponent;
     this.videoElement = null;
     this.sourceBuffer = null;
     this.selected = {};
     this.videoDuration = 0;
+    this.activeItem = {};
+    this.selections = [];
   }
   initializeCodec() {
     this.videoElement = document.getElementById("vid");
@@ -31,14 +34,58 @@ class VideoMediaPlayer {
       const selected = this.selected = this.manifestJSON.intro;
       // evitar rodar como "LIVE"
       mediaSource.duration = this.videoDuration;
-      await this.fileDownload(selected.url)
+      await this.fileDownload(selected.url);
+      setInterval(this.waitForQuestions.bind(this), 200);
     }
   }
 
+  waitForQuestions() {
+    const currentTime = parseInt(this.videoElement.currentTime);
+    const option = this.selected.at === currentTime;
+    if(!option) return;
+    if(this.activeItem.url === this.selected.url) return;
+    this.videoComponent.configureModal(this.selected.options);
+    this.activeItem = this.selected
+  }
+
+  async currentFileResolution() {
+    const LOWEST_RESOLUTION = 144;
+    const prepareUrl = {
+      url: this.manifestJSON.finalizar.url,
+      fileResolution: LOWEST_RESOLUTION,
+      fileResolutionTag: this.manifestJSON.fileResolutionTag,
+      hostTag: this.manifestJSON.hostTag,
+    }
+    const url = this.network.parseManifestURL(prepareUrl);
+    return this.network.getProperResolution(url)
+  }
+  async nextChunk(data){
+    const key = data.toLowerCase();
+    const selected = this.manifestJSON[key];
+    this.selected = {
+      ...selected,
+      at: parseInt(this.videoElement.currentTime + selected.at)
+    }
+
+    this.videoElement.play();
+    await this.fileDownload(selected.url);
+  }
+
+  manageLag(selected){
+    if(!!~this.selections.indexOf(selected.url)){
+      selected.at =+5
+      return
+    }
+
+    this.selections.push(selected.url)
+  }
+
   async fileDownload(url) {
+    const fileResolution = await this.currentFileResolution();
+    console.log('fileResolution',fileResolution);
     const prepareUrl = {
       url,
-      fileResolution: 360,
+      fileResolution: fileResolution,
       fileResolutionTag: this.manifestJSON.fileResolutionTag,
       hostTag: this.manifestJSON.hostTag
     }
@@ -51,7 +98,7 @@ class VideoMediaPlayer {
   setVideoPlayerDuration(finalUrl) {
     const bars = finalUrl.split('/')
     const [name, videoDuration] = bars[bars.length - 1].split('-');
-    this.videoDuration += videoDuration;
+    this.videoDuration += parseFloat(videoDuration);
   }
 
   async processBufferSegments(allSegments) {
